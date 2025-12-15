6570462b5abf81dcf7b45cae7917ef8078a5df51
DoodleGuyy <doodleguyartist@gmail.com>
2025-12-15 19:40:30 +0100

    Remove sentences from static.py HTML content
    Removed several sentences from the HTML content.

--- static/static.py
+++ static/static.py
@@ -2,8 +2,4 @@
 
 HTML = """
 There's a lot of danger during a big storm.
-I’m excited about the future.
-This desk which I bought yesterday is very big.
-My dog, Jake, barked at the mail carrier.
-What's your favorite thing about school?
 """
